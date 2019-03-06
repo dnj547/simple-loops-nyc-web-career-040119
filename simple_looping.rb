69ca966e331f19ea770a2681709be41d3a161448
Danielle Jasper <dnj547@live.com>
2019-03-06 23:25:07 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -12,7 +12,9 @@ def loop_iterator(number_of_times)
   loop do
     counter += 1
     puts "Welcome to Flatiron School's Web Development Course!"
-    break if counter = number_of_times
+    if counter >= number_of_times
+      break
+    end
   end
   phrase = "Welcome to Flatiron School's Web Development Course!"
 
